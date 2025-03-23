ec84e3039503ade1ba69908557fcc61c209a4bd2
Shailesh Bisht <shaileshsinghbisht0411@gmail.com>
2025-03-23 15:37:46 +0530

    wrote getuserhistoryendpoint

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -4,6 +4,7 @@ import { User } from "../models/user.model.js";
 import { uploadOnCloudinary, deleteCloudinary } from "../utils/cloudinary.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import jwt from "jsonwebtoken";
+import mongoose from "mongoose";
 
 const generateAccessAndRefreshTokens = async (userId) => {
   try {
@@ -427,6 +428,60 @@ const getUserChannelprofile = asyncHandler(async (req, res) => {
     .json(new ApiResponse(200, channel[0], "User Channel Fetched Succesfully"));
 });
 
+const getUserHistory = asyncHandler(async (req, res) => {
+  const user = await User.aggregate([
+    {
+      $match: {
+        _id: new mongoose.Types.ObjectId(req.user._id),
+      },
+    },
+    {
+      $lookup: {
+        from: "videos",
+        localField: "watchHistory",
+        foreignField: "_id",
+        as: "watchHistory",
+        pipeline: [
+          {
+            $lookup: {
+              from: "users",
+              localField: "owner",
+              foreignField: "_id",
+              as: "owner",
+              pipeline: [
+                {
+                  $project: {
+                    username: 1,
+                    avatar: 1,
+                    fullName: 1,
+                  },
+                },
+              ],
+            },
+          },
+          {
+            $addFields: {
+              owner: {
+                $first: "$owner",
+              },
+            },
+          },
+        ],
+      },
+    },
+  ]);
+
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(
+        200,
+        user[0].watchHistory,
+        "User History Fetched Sucesfully"
+      )
+    );
+});
+
 export {
   registerUser,
   loginUser,
@@ -438,4 +493,5 @@ export {
   updateAvatar,
   updateUserCoverImage,
   getUserChannelprofile,
+  getUserHistory,
 };
